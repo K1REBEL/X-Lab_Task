733f60a23cdc65016d05b643a1cd7965294ae5fd
K1REBEL <kibraheem47@yahoo.com>
2024-03-04 10:19:09 +0200

    Routes & Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EmpController;
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('employees', EmpController::class);
+Route::resource('jobs', JobController::class);
